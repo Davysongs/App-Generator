2cce98e14f6b7934adc2245d9d0a97f7e0c5aab1
davysongs <davidgsongs@gmail.com>
2024-07-09 18:07:38 +0100

    App set up

---
+++ auth/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from rest_framework import response
+
+# Create your views here.
+def login(request):
+    return response("Working on login")
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import response
+from rest_framework.views import APIView
 
 # Create your views here.
+class ChatViewset(APIView):
+    def get(self):
+        return response("get chat")
+    def get_queryset(self):
+        return super().get_queryset()
+    
